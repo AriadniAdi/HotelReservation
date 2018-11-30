1d22a36c14cee90bfe43894df169aa19a1643068
AriadniAdi <ariadniadi@gmail.com>
2018-11-30 02:45:01 -0200

    adding tests from input user

--- test/userInputHandlerTest.js
+++ test/userInputHandlerTest.js
@@ -1,6 +1,6 @@
 const assert = require("assert");
 const UserInputHandler = require("../userInputHandler");
-const Errors = require("../errors")
+const Errors = require("../errors");
 
 describe("UserInputHandler", () => {
   beforeEach(() => {
@@ -8,22 +8,64 @@ describe("UserInputHandler", () => {
   });
 
   describe("#extractValues", () => {
-    context("when client type is invalid", () => {
+    describe("invalid client", () => {
+      context("when client type is undefined", () => {
+        it("throws an error", () => {
+          assert.throws(() => {
+            this.userInput.extractValues();
+          }, Errors.invalidClientType());
+        });
+      });
+      context("when client type is empty", () => {
+        it("throws an error", () => {
+          assert.throws(() => {
+            this.userInput.extractValues("");
+          }, Errors.invalidClientType());
+        });
+      });
+    });
+    describe("valid client", () => {
+      context("when client type is regular", () => {
+        it("returns a regular client", () => {
+          var inputRegular = this.userInput.extractValues(
+            "regular:16Mar2009(mon)"
+          );
+          assert.equal(inputRegular.clientType, "regular");
+        });
+      });
+      context("when client type is reward", () => {
+        it("returns a reward client", () => {
+          var inputReward = this.userInput.extractValues(
+            "reward:16Mar2009(mon)"
+          );
+          assert.equal(inputReward.clientType, "reward");
+        });
+      });
+    });
+    context("when date is empty", () => {
       it("throws an error", () => {
         assert.throws(() => {
-          this.userInput.extractValues("invalid");
-        }, Errors.invalidClientType());
+          this.userInput.extractValues("regular:");
+        }, Errors.invalidDates());
+      });
+    });
+    context("when there is one date with wrong format", () => {
+      it("throws an error", () => {
+        assert.equal(() => {
+          this.userInput.extractValues("regular:16Mar2009(mon), xxx");
+        }, Errors.invalidDates());
+      });
+    });
+  });
+  describe("valid dates", () => {
+    context("and date is valid", () => {
+      it("returns a valid date", () => {
+        const result = this.userInput.extractValues("reward:16Mar2009(mon)");
+        const expectedDate = new Date(2009, 3, 16);
+        assert.equal(result.date.year, expectedDate.year);
+        assert.equal(result.date.month, expectedDate.month);
+        assert.equal(result.date.day, expectedDate.day);
       });
     });
   });
 });
-
-// Recebe string e
-// retorna objeto com tipo de cliente e array de data
-
-// testar se está retornando objeto com tipo de cliente e array de data
-// se está aceitando string
-// se exibe erro ao enviar input inválido
-// tipo de cliente inválido
-// uma data inválida , retorna erro.
-// todas datas inválidas
--- userInputHandler.js
+++ userInputHandler.js
@@ -1,9 +1,15 @@
 const Errors = require("./errors");
 
 class UserInputHandler {
-  extractValues() {
+  extractValues(value) {
+    const values = value.split(':');
+    const clientType = values ;
+
+
     throw Errors.invalidClientType();
   }
 }
 
 module.exports = UserInputHandler;
+
+/// Regular: 16Mar2009(mon), 17Mar2009(tues), 18Mar2009(wed)
